fcd92aed63e32687a79ff53da7a342215db9c636
Leandro Alfaro <being.leeaa15@gmail.com>
2023-01-19 14:30:36 -0300

    sexto commit

--- aprendiendo-sql/04-dml/where.sql
+++ aprendiendo-sql/04-dml/where.sql
@@ -1,9 +1,69 @@
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Other/SQLTemplate.sql to edit this template
- */
-/**
- * Author:  Leandro
- * Created: 19 ene. 2023
- */
+
+SELECT * FROM usuario WHERE email='leandro@leandro.com';
+
+/* Operadores de comparacion
+    
+    =
+    !=
+    <
+    >
+    <=
+    >=
+    between A and B --ej:
+    in
+    is null
+    is not null
+    like
+    not
+
+*/
+
+
+--1. mostrar nya de todos los usuarios registrados en 2019
+
+SELECT nombre, apellido FROM usuario WHERE YEAR(fecha)='2019';
+
+--1. mostrar nya de todos los usuarios NO registrados en 2019 o su fecha es nula
+
+SELECT nombre, apellido FROM usuario WHERE YEAR(fecha)!='2019' or isnull(fecha);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
